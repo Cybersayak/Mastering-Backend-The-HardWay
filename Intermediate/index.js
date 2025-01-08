2bb0424a13a429940e9fcf83cba2618562cf9413
Sayak5789 <56470816+Sayak5789@users.noreply.github.com>
2025-01-08 22:59:50 +0530

    EJS setup

--- Intermediate/index.js
+++ Intermediate/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Corrected: adding parentheses to invoke express.json() as middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.send('Server is running');
+});
+
+
+app.get('/demo', function(req, res) {
+    res.render('index');
 });
 
 app.listen(3000, function() {
