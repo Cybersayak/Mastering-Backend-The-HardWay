105ae6ec34aec539144df4fe3153d8c87963f156
Sayak5789 <56470816+Sayak5789@users.noreply.github.com>
2025-03-05 21:09:48 +0530

    Intermediate Completed

--- BACKEND_NODE/Intermediate/index.js
+++ BACKEND_NODE/Intermediate/index.js
@@ -5,7 +5,10 @@ const path = require('path');
 // Corrected: adding parentheses to invoke express.json() as middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middle Ware to Use Static Files 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
@@ -17,6 +20,17 @@ app.get('/demo', function(req, res) {
     res.render('index');
 });
 
+// Dynamic Route
+app.get('/profile/:username', function(req, res) {
+    res.send(`Welcome ${req.params.username} to Backend Development `);
+});
+
+app.get('/profile/:username/:role', function(req, res) {
+    res.send(`Welcome ${req.params.username} ,You are a ${req.params.role} `);
+});
+
+
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
 });
+    
