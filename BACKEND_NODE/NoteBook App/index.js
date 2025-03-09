dea8103a90d011daae4b5476dbc304397ab7d602
Sayak5789 <56470816+Sayak5789@users.noreply.github.com>
2025-03-09 23:53:34 +0530

    UI Done

--- BACKEND_NODE/NoteBook App/index.js
+++ BACKEND_NODE/NoteBook App/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express() ;
 const path = require('path');
-
+const fs = require('fs');
 
 app.set('view engine' , 'ejs');
 app.use(express.json());
@@ -9,10 +9,14 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname,"public")))
 
 
-
-
 app.get('/', function (req , res){
-    res.render('index');
+    fs.readdir("./files", function(err , files){
+        res.render('index', {files: files});
+    })
+})
+
+app.post('/add', function (req , res){
+   console.log(req.body);
 })
 
 app.listen(3000);
