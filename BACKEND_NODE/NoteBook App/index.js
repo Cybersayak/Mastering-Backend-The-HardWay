99ab69502fb02f07652a916aa42fc27eb2408446
Sayak5789 <56470816+Sayak5789@users.noreply.github.com>
2025-03-18 15:54:53 +0530

    NoteBook App Completed

--- BACKEND_NODE/NoteBook App/index.js
+++ BACKEND_NODE/NoteBook App/index.js
@@ -15,8 +15,28 @@ app.get('/', function (req , res){
     })
 })
 
+app.get('/file/:filename', function (req , res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err ,filedata){
+        res.render('show', {filename: req.params.filename , filedata: filedata });
+    })
+})
+
+app.get('/edit/:filename', function (req , res){
+    res.render('edit' ,{ filename: req.params.filename});
+})
+
+app.post('/edit', function (req , res){
+    fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}`, function(err){
+        res.redirect("/");
+    })
+}) 
+
 app.post('/add', function (req , res){
-  fs.
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details ,function(err){
+      res.redirect("/")
+  });
 })
 
 app.listen(3000);
+
+
